f95e27c515eccfeaef892db110351a28e716509a
Robert Sandfield <robert.f.sandfield@gmail.com>
2021-05-27 21:54:00 -0700

    Added images closing when clicking outside their bounding box.

--- public/js/results.js
+++ public/js/results.js
@@ -2,9 +2,17 @@ function openModal(id)
 {
     document.getElementById(id).style.display = "block";
     console.log("Opening " + id);
+    window.addEventListener('mousedown', function outsideClick(e){
+        if (!document.getElementById(id).contains(e.target))
+        {
+            hideModal(id);
+            this.removeEventListener('mousedown', outsideClick)
+        }
+    });
 }
 
 function hideModal(id)
 {
     document.getElementById(id).style.display = "none";
+    console.log("Closing " + id);
 }
